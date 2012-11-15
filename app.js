7b87f263afc1bb44d4fc6a590f61f0383d4431e0
Wojtek Majewski <jumski@gmail.com>
2012-11-15 15:43:28 +0100

    data url

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ function handleFileSelect(evt)
 
   var files = evt.target.files;
 
-  onResizeFiles(files, function(file){
-    uploadFile(file);
+  onResizeFiles(files, function(dataURI){
+    var blob = dataURLToBlob(dataURI);
+    uploadFile(blob);
   });
 
   // var output = [];
@@ -53,6 +54,31 @@ function onResizeFiles(files, callback)
     callback(canvas.toDataURL('image/jpg'));
   }
   reader.readAsDataURL(file);
+}
+
+// https://github.com/ebidel/filer.js/blob/master/src/filer.js#L128
+function dataURLToBlob(dataURL) {
+  var BASE64_MARKER = ';base64,';
+  if (dataURL.indexOf(BASE64_MARKER) == -1) {
+    var parts = dataURL.split(',');
+    var contentType = parts[0].split(':')[1];
+    var raw = parts[1];
+
+    return new Blob([raw], {type: contentType});
+  }
+
+  var parts = dataURL.split(BASE64_MARKER);
+  var contentType = parts[0].split(':')[1];
+  var raw = window.atob(parts[1]);
+  var rawLength = raw.length;
+
+  var uInt8Array = new Uint8Array(rawLength);
+
+  for (var i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
+  }
+
+  return new Blob([uInt8Array], {type: contentType});
 }
 
 /**
